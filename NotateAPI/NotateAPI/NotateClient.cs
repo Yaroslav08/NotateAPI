a98b4e925fd536a710eae78c1ffee394a0f1ce96
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-19 22:18:29 +0300

    Init proj

---
+++ NotateAPI/NotateAPI/NotateClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace NotateAPI
+{
+    public class NotateClient
+    {
+        private int Id;
+        private string AccessToken;
+    }
+}
