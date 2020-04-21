e69aee6dbff7b8896c75991d43484646d44b4c9b
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-04-21 17:09:17 +0300

    Edited method from sync to async

--- NotateAPI/NotateAPI.Example/Program.cs
+++ NotateAPI/NotateAPI.Example/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace NotateAPI.Example
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NotateClient client = NotateClient.Instance();
         }
     }
 }
