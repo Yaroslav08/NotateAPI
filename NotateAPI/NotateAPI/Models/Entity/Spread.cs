0fe5ee130d8c29eb11749240f10cd6d14c520fa0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-05-17 13:20:45 +0300

    Added entity spread

---
+++ NotateAPI/NotateAPI/Models/Entity/Spread.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace NotateAPI.Models.Entity
+{
+    public class Spread
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public User From { get; set; }
+        public User To { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? ReadTime { get; set; }
+        public Note Note { get; set; }
+    }
+}
